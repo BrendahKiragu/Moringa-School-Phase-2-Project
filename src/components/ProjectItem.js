82787c19146254d76787d7c005a5a24e3305ba17
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-14 16:25:20 +0300

    changes classname to handle all buttons in project list

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,18 +1,89 @@
-import React from "react";
-import './ProjectItem.css'
+import React, { useState } from "react";
+import "./ProjectItem.css";
 
-function ProjectItem ({project, handleEdit, handleDelete}) {
+function ProjectItem({ project, handleEdit, handleDelete }) {
+  const [editProject, setEditProject] = useState(false);
+  const [editedProject, setEditedProject] = useState({ ...project });
+
+  function handleEditButtonClick() {
+    setEditProject(true);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setEditedProject({ ...editedProject, [name]: value });
+  }
+
+  function handleSaveButtonClick(e) {
+    e.preventDefault();
+    handleEdit(editedProject);
+    setEditProject(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleEdit(editedProject);
+    setEditProject(false);
+  }
+
   return (
     <div className="project-card" key={project.id}>
       <img className="project-image" src={project.image} alt={project.name} />
       <h3>Title: {project.name}</h3>
       <p>Description: {project.description}</p>
-      <p> Languages: {project.languages.join(', ')}</p>
-      <p>Authors: {project.authors.join(', ')}</p>
-      <button onClick={()=>handleEdit(project)} >Edit</button>
-      <button onClick={() => handleDelete(project)} >Delete</button>
+      <p> Languages: {project.languages.join(", ")}</p>
+      <p>Authors: {project.authors.join(", ")}</p>
+
+      {editProject ? (
+        <form className="project-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={editedProject.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="description"
+            value={editedProject.description}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="image"
+            value={editedProject.image}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="languages"
+            value={editedProject.languages.join(", ")}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="authors"
+            value={editedProject.authors.join(", ")}
+            onChange={handleInputChange}
+          />
+          <button
+            className="project-buttons"
+            type="submit"
+            onClick={handleSaveButtonClick}
+          >
+            Save Changes
+          </button>
+        </form>
+      ) : (
+        <button className="project-buttons" onClick={handleEditButtonClick}>
+          Edit Project
+        </button>
+      )}
+      <button className="project-buttons" onClick={() => handleDelete(project)}>
+        Delete
+      </button>
     </div>
-  );    
+  );
 }
 
 export default ProjectItem;
