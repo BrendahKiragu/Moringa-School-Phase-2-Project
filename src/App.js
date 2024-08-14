5277400ce03110aa263d3a5073382c688d632f37
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-14 09:41:36 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import profilepic from './components/images/profilepic.png'
+import profilepic from './components/images/profpic.avif'
 import './App.css';
 import NavBar from './components/NavBar';
 
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <NavBar />
 
-      <h1>Hi, I'm Thriver Coder 03</h1>
-      <p>Full Stack Developer based in Nairobi, Kenya</p>
+      <h1>Hi, We're Thriver Coders 03</h1>
+      <p>Full Stack Developers based in Nairobi, Kenya</p>
       <img src={profilepic} alt="profilepic" />
       <p>Coding Your Dreams into Reality, One Code Line at a Time.</p>
     </div>
