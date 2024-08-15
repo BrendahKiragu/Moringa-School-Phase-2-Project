31b7fbbbdd1f1cbaa0c9ea98b0e5d3db10f2d4ee
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-15 09:44:55 +0300

    removes errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -58,26 +58,3 @@ return (
 }
       
 export default Login;
-import React,{useState} from "react";
-
-function Login () {
-    const [isLoggedIn, setIsLoggeIn] = useState(false)
-
-    const handleAuth = () => {
-        setIsLoggeIn(!isLoggedIn)
-    }
-    return (
-        <div>
-         <button onClick={handleAuth}>
-            {isLoggedIn ? "Logout" : "Login"}
-            </button> 
-            <p>{isLoggedIn ? "Welcome back!" :"Kindly Log in"}
-                </p>  
-        </div>
-        
-
-        
-    )
-}
-
-export default Login;
