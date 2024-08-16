68945648b52b048d7de513bb52825652bb4d70af
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-08-17 01:11:27 +0300

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,19 @@ import "./ProjectItem.css";
 import "./ProjectList.css";
 
 function Login({ setIsLoggedIn }) {
-  const [username, setUsername] = useState(""); // Define username state
-  const [password, setPassword] = useState(""); // Define password state
+  const [username, setUsername] = useState(""); 
+  const [password, setPassword] = useState(""); 
   const [error, setError] = useState("");
 
   const handleAuth = (event) => {
     event.preventDefault();
 
-    // Regular expression to check for letters and numbers
     const regex = /^[a-zA-Z0-9]+$/;
 
     if (regex.test(username) && regex.test(password)) {
-      // If both username and password pass the validation
       setIsLoggedIn(true);
-      setError(""); // Clear any previous error messages
+      setError(""); 
     } else {
-      // If the validation fails, show an error message
       setError(
         "Invalid username or password. Please use only letters and numbers."
       );
