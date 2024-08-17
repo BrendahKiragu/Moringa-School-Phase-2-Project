4120ffd0a7c1d3c86ebbc1df9d2be3fb48280650
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-17 13:32:06 +0300

    media queries for responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import profilepic from './images/profpic.avif';
 function Home() {
   return (
     <div className="Home">
-      <h1>Hi, I'm Thriver Coder 03</h1>
-      <p>Full Stack Developer based in Nairobi, Kenya</p>
+      <h1>Hi, We're Thriver Coders 03</h1>
+      <p>Full Stack Developers based in Nairobi, Kenya</p>
       <img src={profilepic} alt="profilepic" />
       <p>Coding Your Dreams into Reality, One Code Line at a Time.</p>
     </div>
